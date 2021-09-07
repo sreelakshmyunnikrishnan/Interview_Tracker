c5f4b76abdf07f726861a56bf6fc7f90ced8abf2
Sreelakshmy Unnikrishnan <44528546+sreelakshmyunnikrishnan@users.noreply.github.com>
2021-09-07 21:00:47 +0530

    Update settings.py

--- interviewtracker/settings.py
+++ interviewtracker/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
+import django_heroku
+django_heroku.settings(locals())
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
